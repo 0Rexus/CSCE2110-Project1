29e865cde00d51803529b2600c1eab33c588439b
Liam Roche <liamroche06@gmail.com>
2026-09-15 14:51:09 -0500

    Created Cancellation History

---
+++ CancellationHistory.cpp
@@ -0,0 +1,3 @@
+#include "CancellationHistory.h"
+#include <iostream>
+using namespace std;
---
+++ CancellationHistory.h
@@ -0,0 +1,12 @@
+#ifndef CANCELLATION_HISTORY_H
+#define CANCELLATION_HISTORY_H
+
+#include <iostream>
+
+class CancellationHistory {
+    private:
+
+    public:
+};
+
+#endif
