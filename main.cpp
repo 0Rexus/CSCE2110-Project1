5c53432dda1e3caa0d099f4580d4049e069c6b0f
Liam Roche <liamroche06@gmail.com>
2026-09-18 12:35:06 -0500

    Created printMenu()

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <limits>
+using namespace std;
+
+void printMenu() {
+    cout << "\n========== Campus Resource Reservation System ==========\n"
+         << "1. Display all resources\n"
+         << "2. Display resource availability\n"
+         << "3. Create reservation\n"
+         << "4. Cancel reservation\n"
+         << "5. Display active reservations\n"
+         << "6. Add student to waiting list\n"
+         << "7. Process waiting list\n"
+         << "8. Display waiting list\n"
+         << "9. Display cancellation history\n"
+         << "10. Restore most recent cancellation\n"
+         << "0. Exit\n"
+         << "========================================================\n";
+}
+
+int intCheck() {
+
+}
+
+string stringCheck() {
+
+}
 
 int main() {
-    
+    int choice;
+        
+    do {
+        printMenu();
+    } while (choice != 0);
+
+    return 0;
 }
